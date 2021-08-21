c2d2c4098554b1f961453e128cbb9fe2d1addab1
Dana Sugu <dana.sugu@gmail.com>
2021-08-21 08:02:12 +0300

    Update functions.php

--- wp-content/themes/basic-theme/functions.php
+++ wp-content/themes/basic-theme/functions.php
@@ -1,16 +1,13 @@
 <?php
 
 
+function basic_enqueue_styles() {
+  wp_register_style( 'styles', get_template_directory_uri() . '/css/styles.css' );
+  $depedencies = array('styles');
+  wp_enqueue_style( 'basic-style', get_stylesheet_uri(), $depedencies );
 
+}
 
-
-
-
-
-
-
-
-
-
+add_action( 'wp_enqueue', 'basic_enqueue_styles');
 
 ?>
