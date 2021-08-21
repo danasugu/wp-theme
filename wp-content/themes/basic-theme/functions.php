53ed417697b287e039b71559304c0c97502bff80
Dana Sugu <dana.sugu@gmail.com>
2021-08-21 17:06:45 +0300

    cont.

--- wp-content/themes/basic-theme/functions.php
+++ wp-content/themes/basic-theme/functions.php
@@ -1,19 +1,35 @@
 <?php
 
-/* adding CSS */
-function basic_enqueue_styles() {
-  wp_register_style( 'styles', get_template_directory_uri() . '/css/styles.css' );
-  $depedencies = array('styles');
-  wp_enqueue_style( 'basic-style', get_stylesheet_uri(), $depedencies );
 
+// function basic_enqueue_styles() {
+//   wp_register_style( 'styles', get_template_directory_uri() . '/css/styles.css' );
+//   $depedencies = array('styles');
+//   wp_enqueue_style( 'basic-style', get_stylesheet_uri(), $depedencies );
+
+/* adding CSS */
+function scripts()
+{
+    wp_register_style('style', get_template_directory_uri() . '/css/style.css', [], 1, 'all');
+    wp_enqueue_style('style');
+
+    wp_enqueue_script('jquery');
+
+    // wp_register_script('app', get_template_directory_uri() . '/dist/app.js', ['jquery'], 1, true);
+    wp_enqueue_script('app');
 }
+
+add_action('wp_enqueue_scripts', 'scripts');
+
+
+
+// }
 /* adding Javascript */
 // function basic_enqueue_scripts() {
 //   $dependencies = array('javascript');
 //   wp_enqueue_script('bootstrap' get_template_directory_uri() . '/js/scripts.js', $depedencies);
 // }
 
-add_action( 'wp_enqueue', 'basic_enqueue_styles' );
+add_action( 'wp_enqueue', 'scripts' );
 
 // add_action( 'wp_enqueue', 'bassic_enqueue_scripts');
 
--- wp-content/themes/basic-theme/header.php
+++ wp-content/themes/basic-theme/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="<? language_attributes(); ?>">
+<html <?php language_attributes(); ?>">
 
 <head>
   <meta charset="<? bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <?php wp_head(); ?>
 </head>
 
 <body>
