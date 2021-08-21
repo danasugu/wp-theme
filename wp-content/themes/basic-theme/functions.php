6a80fc4d9ebb474b3cf9db489a3e4e14ed95cc4c
Dana Sugu <dana.sugu@gmail.com>
2021-08-21 08:09:50 +0300

    cont.

--- wp-content/themes/basic-theme/functions.php
+++ wp-content/themes/basic-theme/functions.php
@@ -7,11 +7,14 @@ function basic_enqueue_styles() {
   wp_enqueue_style( 'basic-style', get_stylesheet_uri(), $depedencies );
 
 }
+/* adding Javascript */
+// function basic_enqueue_scripts() {
+//   $dependencies = array('javascript');
+//   wp_enqueue_script('bootstrap' get_template_directory_uri() . '/js/scripts.js', $depedencies);
+// }
 
-function basic_enqueue_scripts() {
-  $dependencies = array('javascript');
-  wp_enqueue_script('bootstrap' get_template_directory_uri() . '/js/scripts.js', $depedencies);
-}
-add_action( 'wp_enqueue', 'basic_enqueue_styles');
+add_action( 'wp_enqueue', 'basic_enqueue_styles' );
+
+// add_action( 'wp_enqueue', 'bassic_enqueue_scripts');
 
 ?>
