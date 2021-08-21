7549be5dd06878f4b714b0ba7d7135b4ba472be6
Dana Sugu <dana.sugu@gmail.com>
2021-08-21 11:23:27 +0300

    cont.

---
+++ wp-content/themes/basic-theme/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="<? language_attributes(); ?>">
+
+<head>
+  <meta charset="<? bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="wrapper">
+
+    <div class="header">
+      <img src="images/logo.svg" alt="Basic WP Website">
+    </div>
+
+    <div class="mobile-menu">
+      <img src="images/menu.svg" alt="Menu">Menu
+    </div>
+
+    <!-- <div id="toggleMyMenu">
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">GALLERY</a></li>
+        <li><a href="#">CONTACT</a></li>
+        <li><a href="#">BLOG</a></li>
+        <li><a href="#">PODCAST</a></li>
+        <li><a href="#">DONATE</a></li>
+      </ul>
+    </div> -->
+
+    <div class="slogan">Everything a writer needs.</div>
+
+
+    <div class="container">
