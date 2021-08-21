5f3157131afcca235c79c56ce33a1987f7974bd8
Dana Sugu <dana.sugu@gmail.com>
2021-08-21 17:29:05 +0300

    cont.

---
+++ wp-content/themes/basic-theme/footer.php
@@ -0,0 +1,18 @@
+<div class="footer">
+  <p><a href="#">Privacy Policy</a> / <a href="#">Cookie Policy</a></p>
+  <p>@ Basic WP Theme 2021 / All Rights Reserved </p>
+</div>
+
+
+<? wp_footer(); ?>
+
+<script>
+  function toogleMenu() {
+    var x = document.getElementById("toggleMyMenu");
+      if (x.style === "none") {
+        x.style = "block";
+      } else {
+        x.style.display = "none";
+      }
+  }
+</script>
